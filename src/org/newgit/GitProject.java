65eb5c6fbe0da03f719133401392fd9d9e06f215
RamalingamVelmurugan <velmuruganavr@gmail.com>
2021-10-23 17:42:59 +0530

    Vel

---
+++ src/org/newgit/GitProject.java
@@ -0,0 +1,17 @@
+package org.newgit;
+
+public class GitProject {
+	
+	private void sample() {
+System.out.println("sample");
+	}
+	
+	public static void main(String[] args) {
+	
+		GitProject gitProject = new GitProject();
+		
+		gitProject.sample();
+	}
+	
+
+}
